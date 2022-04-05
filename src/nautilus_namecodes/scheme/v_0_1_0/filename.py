7d443f29901265e0eedd1458a6f2c6a9694fe2cf
Cameron Garnham <me@da2ce7.com>
2022-04-05 23:03:45 +0200

    parse namecodes

--- src/nautilus_namecodes/scheme/v_0_1_0/filename.py
+++ src/nautilus_namecodes/scheme/v_0_1_0/filename.py
@@ -294,6 +294,22 @@ class Gold(Way):
 
 
 @dataclass
+class Extention:
+    """Filename Extention"""
+
+    extention: str = pydantic.Field(
+        None,
+        title="File Extention",
+        min_length=1,
+        regex=r"\A[^\s\t\n\r\f\v\b\0\\\/\"\<\>\|\:\*\?]+\Z",
+    )
+
+    def get_string(self) -> str:
+        """Simple String Getter"""
+        return self.extention
+
+
+@dataclass
 class Filename:
     """The Data that is encoded into a Filename."""
 
@@ -301,6 +317,7 @@ class Filename:
     listing: Listing
     gold: Gold
     type: DataType
+    extention: Extention
 
     def get_filename(self, all_codes: AllCodes) -> str:
         """Return the encoded filename."""
@@ -317,4 +334,6 @@ class Filename:
             f"{'.'.join(map(str,list(zip(*modification_codes))[0]))}"
             "."
             f"{'.'.join(map(str,list(zip(*type_code))[0]))}"
+            "."
+            f"{self.extention.get_string()}"
         )
---
+++ src/nautilus_namecodes/scheme/v_0_1_0/namecode_lookup.py
@@ -0,0 +1,38 @@
+from dataclasses import InitVar, dataclass, field
+from typing import Tuple
+from nautilus_namecodes.namecodes_dataclasses import (
+    AllCodes,
+    BlockCodes,
+    PlaneCodes,
+    SectionCodes,
+)
+
+
+@dataclass
+class NamecodeLookup:
+    """ "A dataclass that allows for looking up of namecodes"""
+
+    all_codes: InitVar[AllCodes]
+
+    namecode: int
+
+    plane: str = field(init=False)
+    block: str = field(init=False)
+    section: str = field(init=False)
+    code: str = field(init=False)
+
+    def __post_init__(self, all_codes: AllCodes):
+
+        plane_codes: PlaneCodes = all_codes.lookup_plane_by_code(self.namecode)
+        self.plane = plane_codes.name
+
+        block_codes: BlockCodes = plane_codes.lookup_block_by_code(self.namecode)
+        self.block = block_codes.name
+
+        section_codes: SectionCodes = block_codes.lookup_section_by_code(self.namecode)
+        self.section = section_codes.name
+
+        code: Tuple[str, Tuple[int, str]] = section_codes.lookup_by_namecode(
+            self.namecode
+        )
+        self.code = code[0]
